e0720c84a9248cbf40961e101288d8ebc27a61ca
jack <jrfisch95@gmail.com>
2023-07-16 15:57:18 -0400

    initial project setup

---
+++ src/validation/pipes/parse-objectid.pipe.ts
@@ -0,0 +1,19 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { isObjectIdOrHexString } from 'mongoose';
+
+@Injectable()
+export class ParseObjectIdPipe implements PipeTransform {
+  transform(value: any, args: ArgumentMetadata) {
+    if (!isObjectIdOrHexString(value)) {
+      throw new BadRequestException(
+        'Validation failed (24 character ObjectId hex string is expected)',
+      );
+    }
+    return value;
+  }
+}
