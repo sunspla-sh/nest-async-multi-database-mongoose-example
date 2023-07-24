dfda9c83aa1b5098b6d37cd827353f43f13ee8fc
jack <jrfisch95@gmail.com>
2023-07-23 23:50:55 -0400

    add full coverage unit testing for owners controller

--- src/owners/owners.controller.spec.ts
+++ src/owners/owners.controller.spec.ts
@@ -2,6 +2,8 @@ import { TestingModule, Test } from '@nestjs/testing';
 import { OwnersController } from './owners.controller';
 import { OwnersService } from './owners.service';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Types } from 'mongoose';
+import { CreateOwnerDto } from './create-owner.dto';
 
 describe('OwnersController', () => {
   let ownersController: OwnersController;
@@ -32,11 +34,35 @@ describe('OwnersController', () => {
     it('should be a method', () => {
       expect(ownersService.findAll).toEqual(expect.any(Function));
     });
+
+    it('should invoke the findAll method of an instance of OwnersService', () => {
+      ownersController.findAll();
+      expect(ownersService.findAll).toHaveBeenCalled();
+    });
+
+    it('should return the result of invoking the findAll method of an instance of OwnersService', () => {
+      expect(ownersService.findAll).toHaveReturnedWith(
+        ownersController.findAll(),
+      );
+    });
   });
 
   describe('findById', () => {
     it('should be a method', () => {
       expect(ownersService.findById).toEqual(expect.any(Function));
+    });
+
+    it('should invoke the findById method of an instance of OwnersService with a string argument', () => {
+      const id = new Types.ObjectId().toString();
+      ownersController.findById(id);
+      expect(ownersService.findById).toHaveBeenCalledWith(id);
+    });
+
+    it('should return the result of invoking the findById method of an instance of OwnersController with a string argument', () => {
+      const id = new Types.ObjectId().toString();
+      expect(ownersService.findById).toHaveReturnedWith(
+        ownersController.findById(id),
+      );
     });
   });
 
@@ -44,11 +70,37 @@ describe('OwnersController', () => {
     it('should be a method', () => {
       expect(ownersService.create).toEqual(expect.any(Function));
     });
+
+    it('should invoke the create method of an instance of OwnersService with a CreateOwnerDto argument', () => {
+      const createOwnerDto = new CreateOwnerDto();
+      ownersController.create(createOwnerDto);
+      expect(ownersService.create).toHaveBeenCalledWith(createOwnerDto);
+    });
+
+    it('should return the result of invoking the create method of an instance of OwnersService with a CreateOwnerDto argument', () => {
+      const createOwnerDto = new CreateOwnerDto();
+      expect(ownersService.create).toHaveReturnedWith(
+        ownersController.create(createOwnerDto),
+      );
+    });
   });
 
   describe('remove', () => {
     it('should be a method', () => {
       expect(ownersService.remove).toEqual(expect.any(Function));
+    });
+
+    it('should invoke the remove method of an instance of OwnersService with a string argument', () => {
+      const id = new Types.ObjectId().toString();
+      ownersController.remove(id);
+      expect(ownersService.remove).toHaveBeenCalledWith(id);
+    });
+
+    it('should return the result of invoking the remove method of an instance of OwnersService with a string argument', () => {
+      const id = new Types.ObjectId().toString();
+      expect(ownersService.remove).toHaveReturnedWith(
+        ownersController.remove(id),
+      );
     });
   });
 });
