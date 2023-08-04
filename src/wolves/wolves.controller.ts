243aebaf7c02ae2e3c154500ef9a6a670ecfdeb5
jack <jrfisch95@gmail.com>
2023-08-03 20:56:43 -0400

    add wolf controller. still need to add return types.

--- src/wolves/wolves.controller.ts
+++ src/wolves/wolves.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { ParseObjectIdHexStringPipe } from '../validation/pipes/parse-objectid-hex-string.pipe';
 import { WolvesService } from './wolves.service';
+import { CreateWolfDto } from './create-wolf.dto';
 
 @Controller('wolves')
 export class WolvesController {
   constructor(private wolvesService: WolvesService) {}
+
+  @Get()
+  findAll() {
+    return this.wolvesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id', ParseObjectIdHexStringPipe) id: string) {
+    return this.wolvesService.findById(id);
+  }
+
+  @Post()
+  create(@Body() createWolfDto: CreateWolfDto) {
+    return this.wolvesService.create(createWolfDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseObjectIdHexStringPipe) id: string) {
+    return this.wolvesService.remove(id);
+  }
 }
