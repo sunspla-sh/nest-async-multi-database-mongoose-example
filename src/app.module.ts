c3a703ee6d5576695fa6f00da1b75efc1955316c
jack <jrfisch95@gmail.com>
2023-08-05 11:14:19 -0400

    add env validation. add async configuration of mongoose modules with validated env variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validate, EnvVariables } from './validation/env/env.validate';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {
   NEST_MULTIDB_OWNERS_AND_CATS_CONNECTION,
   NEST_MULTIDB_WOLVES_CONNECTION,
 } from './constants';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { OwnersModule } from './owners/owners.module';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-multidb-owners-and-cats', {
+    ConfigModule.forRoot({
+      validate,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<EnvVariables>) => ({
+        uri: configService.get('OWNERS_AND_CATS_DB'),
+      }),
       connectionName: NEST_MULTIDB_OWNERS_AND_CATS_CONNECTION,
+      inject: [ConfigService],
     }),
-    MongooseModule.forRoot('mongodb://localhost/nest-multidb-wolves', {
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<EnvVariables>) => ({
+        uri: configService.get('WOLVES_DB'),
+      }),
       connectionName: NEST_MULTIDB_WOLVES_CONNECTION,
+      inject: [ConfigService],
     }),
+
     OwnersModule,
     CatsModule,
   ],
---
+++ src/validation/env/env.validate.ts
@@ -0,0 +1,43 @@
+import { IsEnum, IsString, IsNumber, validateSync } from 'class-validator';
+import { plainToInstance, Transform } from 'class-transformer';
+
+enum Environment {
+  DEV = 'dev',
+  QA = 'qa',
+  STAGING = 'staging',
+  PROD = 'prod',
+}
+
+export class EnvVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsNumber()
+  @Transform(({ value }) => parseInt(value))
+  PORT: number;
+
+  @IsString()
+  OWNERS_AND_CATS_DB: string;
+
+  @IsString()
+  WOLVES_DB: string;
+}
+
+/**
+ * Define a synchronous validate function which attempts to validate our env variables that
+ * have been merged into process.env object. If this function throws an error, it will
+ * prevent our application from bootstrapping.
+ */
+export function validate(config: Record<string, unknown>) {
+  const envVariables = plainToInstance(EnvVariables, config);
+
+  const errors = validateSync(envVariables, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return envVariables;
+}
