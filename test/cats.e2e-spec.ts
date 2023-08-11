efab53f1518eb42655a40492ec5a4838623674f9
jack <jrfisch95@gmail.com>
2023-08-10 21:14:13 -0400

    begin setup of e2e tests for cats domain

---
+++ test/cats.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('CatsController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('GET /cats', () => {
+    return request(app.getHttpServer()).get('/cats').expect(200).expect([]);
+  });
+
+  it('GET /cats/:id', () => {});
+
+  it('POST /cats', () => {});
+
+  it('POST /cats/multiple', () => {});
+
+  it('DELETE /cats/:id', () => {});
+});
