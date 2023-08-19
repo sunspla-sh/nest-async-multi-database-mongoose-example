5e45a2a11089654fc77f242cda628c4ce13d1695
jack <jrfisch95@gmail.com>
2023-08-19 10:10:14 -0400

    finish e2e testing of wolves controller

--- test/wolves.e2e.spec.ts
+++ test/wolves.e2e.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
-import { Model, isObjectIdOrHexString } from 'mongoose';
+import { Model, Types, isObjectIdOrHexString } from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { Wolf } from '../src/wolves/schemas/wolf.schema';
 import { Owner } from '../src/owners/schemas/owner.schema';
@@ -21,7 +21,6 @@ describe('WolvesController (e2e)', () => {
   beforeAll(async () => {
     mongod = await MongoMemoryServer.create();
     const uri = mongod.getUri();
-    console.log(uri);
     const ownersAndCatsDbUri = uri + 'nest-multidb-owners-and-cats';
     process.env.OWNERS_AND_CATS_DB = ownersAndCatsDbUri;
     const wolvesDbUri = uri + 'nest-multidb-wolves';
@@ -82,17 +81,141 @@ describe('WolvesController (e2e)', () => {
       ...JSON.parse(JSON.stringify(firstCreatedWolf.toJSON())),
       owner: createdOwner.id,
     });
+
+    const secondTestWolf: Wolf = {
+      type: 'brown',
+      owner: createdOwner.id,
+    };
+
+    const secondCreatedWolf = await wolfModel.create(secondTestWolf);
+
+    const resTwoWolves = await request(server).get('/wolves');
+    expect(resTwoWolves.statusCode).toBe(200);
+    expect(resTwoWolves.body).toEqual(expect.any(Array));
+    expect(resTwoWolves.body).toContainEqual({
+      ...JSON.parse(JSON.stringify(firstCreatedWolf.toJSON())),
+      owner: createdOwner.id,
+    });
+    expect(resTwoWolves.body).toContainEqual({
+      ...JSON.parse(JSON.stringify(secondCreatedWolf.toJSON())),
+      owner: createdOwner.id,
+    });
+
+    await wolfModel.findByIdAndDelete(firstCreatedWolf.id);
+    await wolfModel.findByIdAndDelete(secondCreatedWolf.id);
+    await ownerModel.findByIdAndDelete(createdOwner.id);
   });
 
   it('GET /wolves/:id', async () => {
-    return;
+    const server = app.getHttpServer();
+
+    const resBadRequest = await request(server).get('/wolves/someFakeId');
+    expect(resBadRequest.statusCode).toBe(400);
+    expect(resBadRequest.body.error).toBe('Bad Request');
+
+    const objectIdButNotAssociatedWithAWolf =
+      new Types.ObjectId().toHexString();
+
+    const resNotFoundRequest = await request(server).get(
+      `/wolves/${objectIdButNotAssociatedWithAWolf}`,
+    );
+    expect(resNotFoundRequest.statusCode).toBe(200);
+    expect(resNotFoundRequest.body).toEqual({});
+
+    const testOwner: Owner = {
+      firstName: 'schlomo',
+      lastName: 'gonzalez',
+    };
+
+    const createdOwner = await ownerModel.create(testOwner);
+
+    const testWolf: Wolf = {
+      type: 'european',
+      owner: createdOwner.id,
+    };
+
+    const createdWolf = await wolfModel.create(testWolf);
+
+    const resFoundWolf = await request(server).get(`/wolves/${createdWolf.id}`);
+
+    expect(resFoundWolf.statusCode).toBe(200);
+    expect(resFoundWolf.body).toEqual({
+      ...JSON.parse(JSON.stringify(createdWolf.toJSON())),
+      owner: JSON.parse(JSON.stringify(createdOwner.toJSON())),
+    });
+
+    await wolfModel.findByIdAndDelete(createdWolf.id);
+    await ownerModel.findByIdAndDelete(createdOwner.id);
   });
 
   it('POST /wolves', async () => {
-    return;
+    const server = app.getHttpServer();
+
+    const resBadRequest = await request(server)
+      .post('/wolves')
+      .send({ junk: 'data' });
+
+    expect(resBadRequest.statusCode).toBe(400);
+    expect(resBadRequest.body.error).toBe('Bad Request');
+
+    const testOwner: Owner = {
+      firstName: 'bobo',
+      lastName: 'jones',
+    };
+
+    const createdOwner = await ownerModel.create(testOwner);
+
+    const testWolf: Wolf = {
+      type: 'asian',
+      owner: createdOwner.id,
+    };
+
+    const resCreatedWolf = await request(server).post('/wolves').send(testWolf);
+
+    expect(resCreatedWolf.statusCode).toBe(201);
+    expect(resCreatedWolf.body).toMatchObject(testWolf);
+    expect(isObjectIdOrHexString(resCreatedWolf.body._id)).toBe(true);
+
+    await ownerModel.findByIdAndDelete(createdOwner.id);
+    await wolfModel.findByIdAndDelete(resCreatedWolf.body._id);
   });
 
   it('DELETE /wolves', async () => {
-    return;
+    const server = app.getHttpServer();
+
+    const resBadRequest = await request(server).delete('/wolves/someFakeId');
+    expect(resBadRequest.statusCode).toBe(400);
+    expect(resBadRequest.body.error).toBe('Bad Request');
+
+    const someValidObjectIdNotAssociatedWithAWolf =
+      new Types.ObjectId().toHexString();
+
+    const resNotFoundRequest = await request(server).delete(
+      `/wolves/${someValidObjectIdNotAssociatedWithAWolf}`,
+    );
+    expect(resNotFoundRequest.statusCode).toBe(200);
+    expect(resNotFoundRequest.body).toEqual({});
+
+    const testOwner: Owner = {
+      firstName: 'abraham',
+      lastName: 'lincoln, vampire slayer',
+    };
+
+    const createdOwner = await ownerModel.create(testOwner);
+
+    const testWolf: Wolf = {
+      type: 'australian',
+      owner: createdOwner.id,
+    };
+
+    const createdWolf = await wolfModel.create(testWolf);
+
+    const resDeleted = await request(server).delete(
+      `/wolves/${createdWolf.id}`,
+    );
+    expect(resDeleted.statusCode).toBe(200);
+    expect(resDeleted.body).toEqual(
+      JSON.parse(JSON.stringify(createdWolf.toJSON())),
+    );
   });
 });
